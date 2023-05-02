fd8e206e503d3e3b7550c1415f0702697880f498
Mary Sanders <marysanders@Marys-MBP.lan>
2023-05-02 18:08:41 -0500

    Finish project

--- lesson1/exercises/src/app/app.module.ts
+++ lesson1/exercises/src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { ChoresListComponent } from './chores-list/chores-list.component';
 import { FavPhotosComponent } from './fav-photos/fav-photos.component';
-import { FavListComponent } from './fav-list/fav-list.component';
+import { FavListComponent } from './fav-links/fav-list.component';
+import { PageTitleComponent } from './page-title/page-title.component';
+import { FavLinksComponent } from './fav-links/fav-links.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { FavListComponent } from './fav-list/fav-list.component';
     MovieListComponent,
     ChoresListComponent,
     FavPhotosComponent,
-    FavListComponent
+    FavListComponent,
+    PageTitleComponent,
+    FavLinksComponent
   ],
   imports: [
     BrowserModule
